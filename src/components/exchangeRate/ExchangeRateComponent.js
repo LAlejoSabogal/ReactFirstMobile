0d5d809419444ae6125315dd43b544ea60693a4c
AlejandroSabogal <lasr9103@hotmail.com>
2021-03-20 00:03:06 -0500

    :art: Adjust table in ExchangeRateStyle Component

--- src/components/exchangeRate/ExchangeRateComponent.js
+++ src/components/exchangeRate/ExchangeRateComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 //Constants
 import TEXT from 'constants/stringConstants'
 import coinValue from 'constants/exchangeTable'
@@ -11,45 +12,57 @@ import Grid from '@material-ui/core/Grid'
 
 const ExchangeRateComponent = () => {
 
+    const [moneda, setMoneda] = useState(true)
+
+    let chageTable = function() {
+        if (!moneda){
+            setMoneda(false)
+        }
+        else{
+            setMoneda(true)
+        }
+    }
+
     const tableCoins = coinValue.map((e,i) => {
         return(
-            <Grid container spacing={0} key={i}>
-                {
-                    e.map(function (ee,ii) {
-                        return(
-                            <React.Fragment key={ii}>
-                                <Grid 
-                                    container
-                                    item
-                                    xs={6}
-                                    alignContent="center"
-                                    justify="flex-start"
-                                >
-                                    {ee.name}
-                                </Grid>
-                                <Grid 
-                                    container
-                                    item
-                                    xs={4}
-                                    alignContent="center"
-                                    justify="center"
-                                >
-                                    {ee.value}
-                                </Grid>
-                                <Grid 
-                                    container
-                                    item
-                                    xs={2}
-                                    alignContent="center"
-                                    justify="center"
-                                >
-                                    <img src={ee.icon} alt="" />
-                                </Grid>
-                            </React.Fragment>
-                        )
-                    })
-                }
-            </Grid>
+            <div className={`${(i===0)?"tableBody":"tableBody--hidden"}`} key={i}>
+                <Grid container spacing={1}>
+                    {
+                        e.map(function (ee,ii) {
+                            return(
+                                <React.Fragment key={ii}>
+                                    <Grid 
+                                        container
+                                        item
+                                        xs={6}
+                                        alignContent="center"
+                                        justify="flex-start"
+                                    >
+                                        <div className="tableBody__cell">
+                                            {ee.name}
+                                        </div>
+                                    </Grid>
+                                    <Grid 
+                                        container
+                                        item
+                                        xs={6}
+                                        alignContent="center"
+                                        alignItems="center"
+                                        justify="center"
+                                    >
+                                        <div className="tableBody__cell">
+                                            {ee.value}
+                                        </div>
+                                        <div>
+                                            <img src={ee.icon} alt="" />
+                                        </div>
+                                    </Grid>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </Grid>
+            </div>
         )
     })
 
@@ -59,10 +72,8 @@ const ExchangeRateComponent = () => {
             <h2>{TEXT.exchangeRate.title}</h2>
             <p className="description">{TEXT.exchangeRate.subtitle}</p>
             <div className="tableContainer">
-                <p>{TEXT.exchangeRate.tableTitle}</p>
-                <div className="tableBody">
-                    {tableCoins}
-                </div>
+                {/* <p>{TEXT.exchangeRate.tableTitle}</p> */}
+                {tableCoins}
             </div>
             <div className="infoStick">
               <p className="infoText">{TEXT.exchangeRate.value} <b>{TEXT.exchangeRate.valueType}</b></p> 
